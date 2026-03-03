5d655bdf0b117cea89d8cda1d4fe2d35f73188f4
tduki <95369488+tduki@users.noreply.github.com>
2026-03-03 16:33:03 +0100

    test: personnage demarre avec 10 HP

--- tests/features/character.feature
+++ tests/features/character.feature
@@ -1,1 +1,5 @@
 Feature: Gestion des personnages
+
+  Scenario: Un nouveau personnage a 10 points de vie
+    Given un nouveau personnage
+    Then le personnage a 10 points de vie
--- tests/test_character.py
+++ tests/test_character.py
@@ -6,3 +6,18 @@ from rpg.character import Character
 @pytest.fixture
 def context():
     return {}
+
+
+@scenario('features/character.feature', 'Un nouveau personnage a 10 points de vie')
+def test_new_character():
+    pass
+
+
+@given("un nouveau personnage")
+def new_character(context):
+    context["char"] = Character("Alice")
+
+
+@then("le personnage a 10 points de vie")
+def check_10hp(context):
+    assert context["char"].health == 10
