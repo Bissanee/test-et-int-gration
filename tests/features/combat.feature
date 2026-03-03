3cc58f7f80d4ef32b231aac06bc351953ef12c0e
tduki <95369488+tduki@users.noreply.github.com>
2026-03-03 16:35:43 +0100

    test: un personnage en attaque un autre

--- tests/features/combat.feature
+++ tests/features/combat.feature
@@ -1,1 +1,6 @@
 Feature: Système de combat
+
+  Scenario: Un personnage en attaque un autre
+    Given deux personnages
+    When l'un attaque l'autre
+    Then l'autre perd 1 point de vie
--- tests/test_combat.py
+++ tests/test_combat.py
@@ -6,3 +6,8 @@ from rpg.character import Character
 @pytest.fixture
 def context():
     return {}
+
+
+@scenario('features/combat.feature', 'Un personnage en attaque un autre')
+def test_attack():
+    pass
