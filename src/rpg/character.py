ce215e740c4efcdce5d3d74cf1c38692871d5947
tduki <95369488+tduki@users.noreply.github.com>
2026-03-03 16:35:24 +0100

    feat: erreur si personnage mort attaque

--- src/rpg/character.py
+++ src/rpg/character.py
@@ -4,6 +4,8 @@ class Character:
         self.health = 10
 
     def attack(self, other: "Character") -> None:
+        if self.is_dead():
+            raise ValueError(f"{self.name} est mort et ne peut pas attaquer.")
         other.health = max(0, other.health - 1)
 
     def is_dead(self) -> bool:
