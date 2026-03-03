c0ad50985f69219223fcb9f7a43e12c5241d4087
tduki <95369488+tduki@users.noreply.github.com>
2026-03-03 16:33:40 +0100

    test: personnage meurt a 0 HP

--- tests/features/character.feature
+++ tests/features/character.feature
@@ -3,3 +3,8 @@ Feature: Gestion des personnages
   Scenario: Un nouveau personnage a 10 points de vie
     Given un nouveau personnage
     Then le personnage a 10 points de vie
+
+  Scenario: Un personnage meurt à 0 point de vie
+    Given un nouveau personnage
+    When le personnage perd toute sa vie
+    Then le personnage est mort
--- tests/test_character.py
+++ tests/test_character.py
@@ -13,11 +13,26 @@ def test_new_character():
     pass
 
 
+@scenario('features/character.feature', 'Un personnage meurt à 0 point de vie')
+def test_character_dies():
+    pass
+
+
 @given("un nouveau personnage")
 def new_character(context):
     context["char"] = Character("Alice")
 
 
+@when("le personnage perd toute sa vie")
+def lose_all_health(context):
+    context["char"].health = 0
+
+
 @then("le personnage a 10 points de vie")
 def check_10hp(context):
     assert context["char"].health == 10
+
+
+@then("le personnage est mort")
+def check_dead(context):
+    assert context["char"].is_dead()
