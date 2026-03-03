a4307d0208f61bdf382297287977bcfdcca50026
tduki <95369488+tduki@users.noreply.github.com>
2026-03-03 16:36:27 +0100

    feat: combat mutuel

--- tests/test_combat.py
+++ tests/test_combat.py
@@ -29,6 +29,17 @@ def attack(context):
     context["attacker"].attack(context["defender"])
 
 
+@when("l'autre riposte")
+def attack_back(context):
+    context["defender"].attack(context["attacker"])
+
+
 @then("l'autre perd 1 point de vie")
 def check_damage(context):
+    assert context["defender"].health == 9
+
+
+@then("les deux personnages ont 9 points de vie")
+def check_mutual_damage(context):
+    assert context["attacker"].health == 9
     assert context["defender"].health == 9
