8ecf1d147a238181d0b4d0ea861e73d81b646512
tduki <95369488+tduki@users.noreply.github.com>
2026-03-04 09:24:29 +0100

    feat: HP calcules selon 10 plus endurance

--- src/rpg/character.py
+++ src/rpg/character.py
@@ -2,7 +2,7 @@ class Character:
     def __init__(self, name: str, endurance: int = 0):
         self.name = name
         self.endurance = endurance
-        self.health = 10
+        self.health = 10 + endurance
 
     def attack(self, other: "Character") -> None:
         if self.is_dead():
